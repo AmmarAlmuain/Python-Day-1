9da5aac1ec7381ce4b1baeafb978405a8f7c8a7d
AmmarAlmuain <masterofalune@gmail.com>
2024-08-22 01:16:54 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Python-Day-1" 
---
+++ main.py
@@ -0,0 +1,150 @@
+# # * Let's try to make calculator together, all you need to do is repair the code or fill the missing!
+
+
+# # TODO: I want num_1 and numb_2 variables to store int data type not string so fix this line to match the needs!
+num_1 = input()
+num_2 = input()
+operation = input("Enter the opration you want : ")
+
+# # * Try to understand the arguments of each function carefully the add function have two arguments (num_1, num_2)
+
+
+def add(num_1, num_2):
+    print(num_1 + num_2)
+
+# # TODO: Continue making functions for each operation until you done 4 currect (working!) functions!
+
+
+def calaulator(num_1, num_2, operation):
+    # # TODO: Fix the condition underneath!
+    if operation != "ha":
+        add(num_1, num_2)
+    if operation == "-":
+        pass
+        # # * uncomment the function below, when you done making the function first!
+        # sub(num_1, num_2)
+    # TODO: Link each function with its currect condition!
+
+
+calaulator(num_1, num_2, operation)
+
+# # ? You might use these links for help!
+# # * https://www.w3schools.com/python/python_conditions.asp
+# # * https://www.w3schools.com/python/python_functions.asp
+# # * https://www.w3schools.com/python/ref_func_input.asp
+
+# # ?  Instructions: 1 - First repair the first two variable to handle int (numbers)!
+# # ?                       2 - Create the missing functions up to 4 operations (don't forget what is arguments)!
+# # ?                       3 - Link the functions with there parent (calculator function) take carefull look into what conditions you need to get the user with the operation he/she want!
+
+
+# # ! Solution is down but try your best befoe looking down!
+
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+# # * down
+
+"""
+num_1 = int(input("Enter first number : "))
+num_2 = int(input("Enter second number : "))
+operation = input("Enter the opration you want : ")
+
+
+def add(num_1, num_2):
+    print(num_1 + num_2)
+
+
+def sub(num_1, num_2):
+    print(num_1 - num_2)
+
+
+def multiply(num_1, num_2):
+    print(num_1 * num_2)
+
+
+def divide(num_1, num_2):
+    print(num_1 * num_2)
+
+
+def calaulator(num_1, num_2, operation):
+    if operation == "+":
+        add(num_1, num_2)
+    if operation == "-":
+        sub(num_1, num_2)
+    if operation == "*":
+        multiply(num_1, num_2)
+    if operation == "/":
+        divide(num_1, num_2)
+
+
+calaulator(num_1, num_2, operation)
+"""
